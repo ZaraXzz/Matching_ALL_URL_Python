e1cbca881b703e5fc3f3e3ef594dc7dfa58bc7f8
zaraxzz <a1-6@ZaraXzzMac.local>
2024-09-28 14:59:49 +0800

    research

--- find_all_url.py
+++ find_all_url.py
@@ -8,15 +8,15 @@
 #
 ####################################################
 import re
-
+import requests
 
 def find_all_url(sentence, show_urls = None, delete_urls = None):
     r = re.compile(r'(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))')
     url_list = r.findall(sentence)
     if show_urls == 1:
-        print "find", str(len(url_list)), "URLs"
+        print("find," + str(len(url_list)))
         for i in url_list:
-            print i[0]
+            print(i[0])
 
     if delete_urls == 1:
         for j in url_list:
@@ -27,22 +27,27 @@ def find_all_url(sentence, show_urls = None, delete_urls = None):
 
 
 
+def download_webpage(url):
+    try:
+        # 发送GET请求
+        response = requests.get(url)
+        # 确保请求成功
+        response.raise_for_status()
+        # 返回网页内容
+        return response.text
+    except requests.RequestException as e:
+        # 打印错误信息
+        print(f"Error downloading the webpage: {e}")
+        return None
+
 
 
 
 if __name__ == '__main__':
-    s1 = '很急很关键 www.whu.edu.cn'
-    s2 = '在下头很硬 http://cs.whu.edu.cn'
-    s3 = 'http://goo.gl/BmT8gZ 匹配不到就删除python了https://goo.gl/MxRdMO'
-    s4 = '我的邮箱是Emal:luochuwei@gmail.com 也可以联系我的武汉大学邮箱，但是不告诉你^_^'
-    
-    find_all_url(s4)
-    print find_all_url(s4,delete_urls=1)
-    find_all_url(s4,show_urls=1)
-    print '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
-    find_all_url(s3)
-    print find_all_url(s3,delete_urls=1)
-    find_all_url(s3,show_urls=1)
+    # 使用函数
+    url = 'https://www.bbc.com'  # 替换为你想要下载的网页的URL
+    webpage_content = download_webpage(url)
+    find_all_url(webpage_content, show_urls=1)
 
 
 
